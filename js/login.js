1749c3dd246f8db3f257dcf11a058c69b678de5f
Arthur Emanuel <arthurresende2004@gmail.com>
2024-08-23 02:14:08 -0300

    Finalizando login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,28 @@
 document.getElementById("meu-botao").onclick = function() {
     window.location.href = "../html/home.html";
 };
+
+async function loginUser(event) {
+    event.preventDefault();
+    const usuario = document.getElementById('usuario').value;
+    const senha = document.getElementById('senha').value;
+
+    const response = await fetch('https://localhost:7237/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ usuario, senha })
+    });
+
+    const data = await response.json();
+    if (data.success) {
+        console.log('Login bem-sucedido');
+
+    } else if (!data.success){
+        alert("Usuário ou senha incorreto!")
+    }
+
+}
+
+document.getElementById('form-login').addEventListener('submit', loginUser);
